812b576db14a95f64b485d8a63078fd983e30bce
soushians noorghorbani <snoorghorbani@gmail.com>
2018-02-14 00:56:53 +0330

    1.0.31

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-exports.InfraModule = require("./lib/config.module");
+exports.InfraModule =   require("./lib/config.module");
+exports.reducers    =   require("./lib/reducers");
+exports.services    =   require("./lib/services");
